11deec39ccc6ccb09a3062c2d3c681b9512a4f85
Vivien Tang <vivientang@Viviens-MacBook-Pro.local>
2017-05-15 11:19:54 +0100

    add deposit and withdraw methods

--- lib/account.rb
+++ lib/account.rb
@@ -6,9 +6,18 @@ class Account
     @transactions = transactions
   end
 
+  def deposit(amount)
+    self.balance += amount
+  end
+
+  def withdraw(amount)
+    self.balance -= amount
+  end
+
   def read_statement
     self.transactions
   end
+
 
   private
   attr_writer :balance
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -5,7 +5,7 @@ describe Account do
   subject(:account) { described_class.new(transactions) }
   let(:transactions) { [] }
 
-  describe 'initialization' do
+  describe '#initialize' do
     it 'new accounts have a balance of 0' do
       expect(account.balance).to eq(0)
     end
@@ -14,4 +14,27 @@ describe Account do
       expect(account.read_statement).to eq([])
     end
   end
+
+  context "user has positive balance" do
+
+    before do
+      account.deposit(100)
+    end
+
+    describe '#deposit' do
+      it 'allows user to deposit an amount into their account' do
+        account.deposit(100)
+        expect(account.balance).to eq(200)
+      end
+    end
+
+    describe '#withdraw' do
+      it 'allows user to withdraw an amount from their account' do
+        account.withdraw(50)
+        expect(account.balance).to eq(50)
+      end
+    end
+
+  end
+
 end
